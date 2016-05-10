97858a01190b593a444518cf2ee656ab0f4f72b0
Peter Szel <szelpeter@szelpeter.hu>
2016-05-11 01:10:28 +0200

    Added Powershell file to create the release zip file.

---
+++ create-release-zip.ps1
@@ -0,0 +1,22 @@
+﻿
+mkdir woocommerce-barion
+cd woocommerce-barion
+
+git clone --quiet git@github.com:szelpe/woocommerce-barion.git .
+
+git submodule --quiet init
+git submodule --quiet update
+
+rm -Force -Recurse .git
+rm barion-library\.git
+rm .gitignore
+rm .gitmodules
+rm Gruntfile.js
+rm create-release-zip.ps1
+
+cd ..
+
+$env:Path += $env:Path + ";C:\Program Files\7-Zip"
+7z a woocommerce-barion.zip woocommerce-barion
+
+rm -Force -Recurse woocommerce-barion
