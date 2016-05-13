32735e02eaff05d4e362f278ca817e05a9730dd4
Peter Szel <szelpeter@szelpeter.hu>
2016-05-13 23:55:33 +0200

    Added helper script to update wordpress.org SVN package.

---
+++ build/wordpress.org-helper.ps1
@@ -0,0 +1,13 @@
+param (
+    [string]$version
+)
+
+svn checkout https://plugins.svn.wordpress.org/pay-via-barion-for-woocommerce/
+
+rm -Recurse -Force pay-via-barion-for-woocommerce\trunk\*
+cp woocommerce-barion\* pay-via-barion-for-woocommerce\trunk\
+
+svn ci -m "Added version $version"
+
+svn cp trunk tags/$version
+svn ci -m "tagging version $version"
