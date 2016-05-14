e83fcf0f42d6ef7fc1c8a8ec31f4f9de4d515f2c
Peter Szel <szelpeter@szelpeter.hu>
2016-05-14 02:16:27 +0200

    Fixed wordpress.org helper script.

--- build/wordpress.org-helper.ps1
+++ build/wordpress.org-helper.ps1
@@ -7,8 +7,12 @@ svn checkout https://plugins.svn.wordpress.org/pay-via-barion-for-woocommerce/
 rm -Recurse -Force pay-via-barion-for-woocommerce\trunk\*
 cp woocommerce-barion\* pay-via-barion-for-woocommerce\trunk\
 
-svc add pay-via-barion-for-woocommerce\trunk\*
+cd pay-via-barion-for-woocommerce
+
+svn add .\trunk\* --force
 svn ci -m "Added version $version"
 
 svn cp trunk tags/$version
 svn ci -m "tagging version $version"
+
+cd ..
