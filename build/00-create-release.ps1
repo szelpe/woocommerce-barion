0939793b8faff7a34a9a0a7f494c93bd3935ec38
Peter Szel <peter.szel@greenfoxacademy.com>
2019-09-01 16:28:40 +0200

    Improved build script

--- build/00-create-release.ps1
+++ build/00-create-release.ps1
@@ -4,6 +4,13 @@ param (
 
 $ErrorActionPreference = "Stop"
 $result = & $PSScriptRoot"\version-bump.ps1" $type
+Write-Output 'New version:' $result.version
+
+$decision = $Host.UI.PromptForChoice('', 'Are you sure you want to proceed?', ('&Yes', '&No'), 1)
+if ($decision -eq 1) {
+    exit
+}
+
 & $PSScriptRoot"\create-release-zip.ps1"
 & $PSScriptRoot"\create-github-release.ps1" $result.version
 & $PSScriptRoot"\wordpress.org-helper.ps1" $result.version
