fcaeac24e31204dd3d54bf6c789312b401c12e8b
Peter Szel <szelpeter@szelpeter.hu>
2022-11-06 09:12:44 +0100

    Fixed indentation that caused issues with PHP < 7.4

--- includes/class-wc-gateway-barion-pixel.php
+++ includes/class-wc-gateway-barion-pixel.php
@@ -25,29 +25,26 @@ class WC_Gateway_Barion_Pixel {
         $barion_pixel_id = htmlspecialchars($this->barion_pixel_id, ENT_QUOTES);
 
         echo <<<EOL
-        <script>
-            // Create BP element on the window
-            window["bp"] = window["bp"] || function () {
-                (window["bp"].q = window["bp"].q || []).push(arguments);
-            };
-            window["bp"].l = 1 * new Date();
-    
-            // Insert a script tag on the top of the head to load bp.js
-            scriptElement = document.createElement("script");
-            firstScript = document.getElementsByTagName("script")[0];
-            scriptElement.async = true;
-            scriptElement.src = 'https://pixel.barion.com/bp.js';
-            firstScript.parentNode.insertBefore(scriptElement, firstScript);
-            window['barion_pixel_id'] = '{$barion_pixel_id}';            
-
-            // Send init event
-            bp('init', 'addBarionPixelId', window['barion_pixel_id']);
-        </script>
-
-        <noscript>
-            <img height="1" width="1" style="display:none" alt="Barion Pixel" src="https://pixel.barion.com/a.gif?ba_pixel_id='{$barion_pixel_id}'&ev=contentView&noscript=1">
-        </noscript>        
-    EOL;
+<script>
+    // Create BP element on the window
+    window["bp"] = window["bp"] || function () {
+        (window["bp"].q = window["bp"].q || []).push(arguments);
+    };
+    window["bp"].l = 1 * new Date();
+    // Insert a script tag on the top of the head to load bp.js
+    scriptElement = document.createElement("script");
+    firstScript = document.getElementsByTagName("script")[0];
+    scriptElement.async = true;
+    scriptElement.src = 'https://pixel.barion.com/bp.js';
+    firstScript.parentNode.insertBefore(scriptElement, firstScript);
+    window['barion_pixel_id'] = '{$barion_pixel_id}';            
+    // Send init event
+    bp('init', 'addBarionPixelId', window['barion_pixel_id']);
+</script>
+<noscript>
+    <img height="1" width="1" style="display:none" alt="Barion Pixel" src="https://pixel.barion.com/a.gif?ba_pixel_id='{$barion_pixel_id}'&ev=contentView&noscript=1">
+</noscript>
+EOL;
     }
 
     /**
